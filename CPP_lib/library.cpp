ca1c87bdb78746e283021850b9e307d86d33f3b5
SoliareofAstora <piotr1kucharski@gmail.com>
2021-06-01 11:50:25 +0200

    c++ load contact map

--- CPP_lib/library.cpp
+++ CPP_lib/library.cpp
@@ -55,17 +55,18 @@ class ContactMapper{
 
     for (int group_a = 0; group_a < seq_size; ++group_a) {
       for (int group_b = group_a + 1; group_b < seq_size; ++group_b) {
-
         bool group_connected = false;
 
         for (int atom_i = start_group_index[group_a]; atom_i < start_group_index[group_a + 1]; ++atom_i) {
           for (int atom_j = start_group_index[group_b]; atom_j < start_group_index[group_b + 1]; ++atom_j) {
+
             if (distance(atom_position, atom_i, atom_j) <= 36.0f) {
               group_connected = true;
               auto bit_index = int(bits_size - (seq_size - group_a) * ((seq_size - group_a) - 1) / 2 + group_b - group_a - 1);
               bit_set->set_bit(bit_index);
               break;
             }
+
           }
           if (group_connected) {
             break;
@@ -80,37 +81,33 @@ class ContactMapper{
     writer.close();
   }
 
-  np::ndarray LoadCmap(std::string path) {
+  np::ndarray LoadCmap(const std::string& path) {
     std::ifstream reader(path, std::ios::in | std::ios::binary | std::ios::ate);
-
-    int bytes_size = reader.tellg();
+    auto bytes_size = reader.tellg();
     if (bit_set->size < bytes_size) {
       delete bit_set;
       bit_set = new BitSet(bytes_size*8);
     }
     std::memset(bit_set->data, 0, bytes_size);
-    int n = int((1 + sqrtf(64 * bytes_size + 1)) / 2);
 
+    int n = int((1 + sqrt(64 * bytes_size + 1)) / 2);
     reader.seekg(0, std::ios::beg);
     reader.read((char*) bit_set->data, bytes_size);
     reader.close();
 
     bool * const data = new bool[n * n];
-    for (int k = 0; k < int(n * (n - 1) / 2); ++k) {
-      if (bit_set->get_bit(k)) {
-        int i = int(n - 2 - int(sqrtf(-8 * k + 4 * n * (n - 1) - 7) / 2.0 - 0.5));
-        int j = int(k + i + 1 - n * (n - 1) / 2 + (n - i) * ((n - i) - 1) / 2);
-        data[i * n + j] = true;
-        data[j * n + i] = true;
-      }
-    }
+    std::memset(data, 0, n * n);
+
     for (int i = 0; i < n; ++i) {
       data[i*n + i] = true;
+      for (int j = i+1; j < n; ++j) {
+        auto bit_index = int(int((n * (n - 1)) / 2) - (n - i) * ((n - i) - 1) / 2 + j - i - 1);
+        if (bit_set->get_bit(bit_index)) {
+          data[i * n + j] = true;
+          data[j * n + i] = true;
+        }
+      }
     }
-
-    np::dtype dt = np::dtype::get_builtin<bool>();
-    py::tuple shape = py::make_tuple(n, n);
-    py::tuple stride = py::make_tuple(sizeof(bool) * n, sizeof(bool));
 
     // https://stackoverflow.com/questions/57068443/setting-owner-in-boostpythonndarray-so-that-data-is-owned-and-managed-by-pyt
     PyObject* capsule = ::PyCapsule_New((void*)data, nullptr, (PyCapsule_Destructor)&DestroyCapsule);
@@ -118,9 +115,9 @@ class ContactMapper{
     py::object capsule_owner{capsule_handle};
 
     return np::from_data(data,
-                         dt,
-                         shape,
-                         stride,
+                         np::dtype::get_builtin<bool>(),
+                         py::make_tuple(n, n),
+                         py::make_tuple(sizeof(bool) * n, sizeof(bool)),
                          capsule_owner
                          );
   }
