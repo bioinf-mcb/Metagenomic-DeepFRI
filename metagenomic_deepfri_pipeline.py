b8db2acb3769ead78fa196b5dea6049a6c21511a
SoliareofAstora <piotr1kucharski@gmail.com>
2022-01-13 13:07:58 +0100

    Added time handler to measure main pipeline runtime

--- metagenomic_deepfri_pipeline.py
+++ metagenomic_deepfri_pipeline.py
@@ -7,20 +7,27 @@ from CONFIG.FOLDER_STRUCTURE import SEQ_ATOMS_DATASET_PATH, DEEPFRI_MODEL_WEIGHT
 
 from CPP_lib.libAtomDistanceIO import initialize as initialize_cpp_lib
 from CPP_lib.libAtomDistanceIO import load_aligned_contact_map
+
+from utils.elapsed_time_handler import ElapsedTimeHandler
 from utils.run_mmseqs_search import run_mmseqs_search
 from utils.search_alignments import search_alignments
 from utils.seq_file_loader import SeqFileLoader
 
 
 def metagenomic_deepfri_pipeline(query_file, target_db, work_path, contact_threshold, generated_contact):
+    elapsed_time_handler = ElapsedTimeHandler(work_path / "metadata_runtime.txt")
+
     mmseqs_search_output = run_mmseqs_search(query_file, target_db, work_path)
+    elapsed_time_handler.log("mmseqs2")
 
     with open(query_file, "r") as f:
         query_seqs = {record.id: record.seq for record in SeqIO.parse(f, "fasta")}
     target_seqs = SeqFileLoader(SEQ_ATOMS_DATASET_PATH)
 
-    # format: alignments[query_id] = {"target_id": target_id, "alignment": alignment}
+    # format: alignments[query_id] = {target_id, identity, alignment[seqA = query_seq, seqB = target_seq, score, start, end]}
+    elapsed_time_handler.reset()
     alignments = search_alignments(query_seqs, mmseqs_search_output, target_seqs, work_path)
+    elapsed_time_handler.log("alignments")
     unaligned_queries = query_seqs.keys() - alignments.keys()
 
     with open(DEEPFRI_MODEL_WEIGHTS_JSON_PATH) as json_file:
@@ -32,6 +39,7 @@ def metagenomic_deepfri_pipeline(query_file, target_db, work_path, contact_thres
     if len(unaligned_queries) > 0:
         print(f"Using CNN for {len(unaligned_queries)} proteins")
     initialize_cpp_lib()
+    elapsed_time_handler.reset()
 
     # mf = molecular_function
     # bp = biological_process
@@ -57,6 +65,7 @@ def metagenomic_deepfri_pipeline(query_file, target_db, work_path, contact_thres
 
             gcn.export_csv(work_path / f"results_gcn_{mode}.csv", verbose=False)
             del gcn
+            elapsed_time_handler.log(f"DeepFri_GCN_{mode}")
 
         if len(unaligned_queries) > 0:
             cnn_params = models_config["cnn"]["models"][mode]
@@ -66,3 +75,4 @@ def metagenomic_deepfri_pipeline(query_file, target_db, work_path, contact_thres
 
             cnn.export_csv(work_path / f"results_cnn_{mode}.csv", verbose=False)
             del cnn
+            elapsed_time_handler.log(f"DeepFri_CNN_{mode}")
---
+++ utils/elapsed_time_handler.py
@@ -0,0 +1,16 @@
+import time
+
+
+class ElapsedTimeHandler(object):
+    def __init__(self, path):
+        self.path = path
+        self.start = time.time()
+
+    def log(self, name):
+        with open(self.path, "a") as f:
+            f.write(f"{name}, {time.time() - self.start}\n")
+        self.reset()
+
+    def reset(self):
+        self.start = time.time()
+
