f2e7dc8e2e5911f7d9edf03e662823be61f9e698
SoliareofAstora <piotr1kucharski@gmail.com>
2022-01-13 12:47:02 +0100

    data format description and aligned sequences identity

--- utils/search_alignments.py
+++ utils/search_alignments.py
@@ -9,6 +9,12 @@ from CONFIG.RUNTIME_PARAMETERS import *
 from utils.seq_file_loader import SeqFileLoader
 
 
+def alignment_sequences_identity(alignment):
+    matches = [alignment.seqA[i] == alignment.seqB[i] for i in range(len(alignment.seqA))]
+    seq_id = (100 * sum(matches)) / len(alignment.seqA)
+    return seq_id
+
+
 def align(query_seq, target_seq):
     best_alignment = None
     best_score = -1
@@ -17,13 +23,22 @@ def align(query_seq, target_seq):
         if alignment.score > best_score:
             best_score = alignment.score
             best_alignment = alignment
-    # save identity score of output
     if best_alignment is not None:
         return best_alignment
     return None
 
 
 def search_alignments(query_seqs: dict, mmseqs_search_output: pd.DataFrame, target_seqs: SeqFileLoader, work_path: pathlib.Path):
+    # format of output JSON file:
+    # alignments = dict[query_id]
+    #     "target_id": target_id,
+    #     "identity" : alignment_sequence_identity(alignment)
+    #     "alignment": alignment = biopython.alignment
+    #         seqA = query_sequence
+    #         seqB = target_sequence
+    #         score = biopython alignment score
+    #         start and end of alignment
+
     json_file = work_path / "alignments.json"
     if json_file.exists():
         return json.load(open(json_file, "r"))
@@ -41,11 +56,11 @@ def search_alignments(query_seqs: dict, mmseqs_search_output: pd.DataFrame, targ
         target_id = mmseqs_search_output["target"].iloc[i]
 
         if query_id not in query_alignments.keys():
-            query_alignments[query_id] = {"target_id": target_id, "alignment": alignment}
+            query_alignments[query_id] = {"target_id": target_id, "alignment": alignment, "identity": alignment_sequences_identity(alignment)}
             continue
 
         if alignment.score > query_alignments[query_id]["alignment"].score:
-            query_alignments[query_id] = {"target_id": target_id, "alignment": alignment}
+            query_alignments[query_id] = {"target_id": target_id, "alignment": alignment, "identity": alignment_sequences_identity(alignment)}
 
-    json.dump(query_alignments, open(json_file, "w"))
+    json.dump(query_alignments, open(json_file, "w"),  indent=4, sort_keys=True)
     return query_alignments
