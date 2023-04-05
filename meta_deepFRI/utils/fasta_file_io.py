31d2248f540a2065b89435c5e08c3cb923dde01e
valentynbez <walentyn.bzsz@gmail.com>
2023-04-05 11:05:11 +0200

    test: fasta file operations
    - refractor - merged encoding operations and I/O

--- meta_deepFRI/utils/fasta_file_io.py
+++ meta_deepFRI/utils/fasta_file_io.py
@@ -1,3 +1,4 @@
+import hashlib
 import dataclasses
 import pathlib
 
@@ -13,6 +14,15 @@ class SeqRecord:
 
 
 def load_fasta_file(file):
+    """
+    Loads FASTA file.
+
+    Args:
+        file (str): path to a FASTA file.
+
+    Returns:
+        List[SeqRecord]: list of records from FASTA file.
+    """
     seq_records = []
     with open(file, "r") as f:
         for line in f:
@@ -25,9 +35,23 @@ def load_fasta_file(file):
 
 
 def write_fasta_file(seq_records, path):
-    with open(path, "w") as f:
-        for seq_record in seq_records:
-            f.write(f">{seq_record.id}\n{seq_record.seq}\n")
+    """Writes a FASTA file
+
+    Args:
+        seq_records (List[SeqRecords]): list of FASTA records.
+        path (str): path to the output file.
+
+    Returns:
+        None
+    """
+    try:
+        with open(path, "w", encoding="utf-8") as f:
+            for seq_record in seq_records:
+                f.write(f">{seq_record.id}\n{seq_record.seq}\n")
+    except IsADirectoryError as e:
+        raise IsADirectoryError(f"Path {path} is a directory. Please provide a path to a file.") from e
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"Path {path} does not exist. Please provide a valid path.") from e
 
 
 class SeqFileLoader:
@@ -43,3 +67,31 @@ class SeqFileLoader:
         with open(self.path / SEQUENCES / (target_id + ".faa"), "r") as f:
             sequence = SeqIO.read(f, "fasta").seq
         return sequence
+
+
+def hash_sequence_id(sequence: str):
+    """Return the SHA256 encoding of protein sequence
+
+    Args:
+        sequence (str): Aminoacid sequence of the protein.
+
+    Returns:
+        str: SHA256 encoding of the protein sequence.
+    """
+    return hashlib.sha256(bytes(sequence, encoding='utf-8')).hexdigest()
+
+
+def encode_faa_ids(path):
+    seq_records = load_fasta_file(path)
+    hash_lookup_dict = {}
+
+    for i in range(len(seq_records)):
+        seq_id_hash = hash_sequence_id(seq_records[i].id)
+        while seq_id_hash in hash_lookup_dict.keys():
+            seq_id_hash = hash_sequence_id(seq_records[i].id + "1")
+        hash_lookup_dict[seq_id_hash] = seq_records[i].id
+        seq_records[i].id = seq_id_hash
+
+    faa_hashed_ids_path = str(path) + ".hashed_ids"
+    write_fasta_file(seq_records, faa_hashed_ids_path)
+    return faa_hashed_ids_path, hash_lookup_dict
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,8 +2,11 @@ import time
 import pytest
 import tempfile
 import os.path
+import hashlib
+from pathlib import Path
 
-from meta_deepFRI.utils import (bio_utils, elapsed_time_logger)
+from meta_deepFRI.utils import (bio_utils, elapsed_time_logger, fasta_file_io)
+# hash_sequence_id, encode_faa_ids, load_fasta_file, write_fasta_file
 
 
 def test_protein_letters():
@@ -90,3 +93,101 @@ def test_elapsed_time_logger():
 
     # Delete the temporary file
     os.remove(tmp_file_path)
+
+
+@pytest.fixture
+def fasta_path(tmpdir_factory):
+    """Create a temporary FASTA file and return its path."""
+    # Create a temporary directory
+    temp_dir = tmpdir_factory.mktemp("fasta_files")
+    # Create a temporary FASTA file
+    fasta_file = temp_dir.join("test.fasta")
+    fasta_file.write(">seq1\nATGC\n>seq2\nCGTA")
+    return str(fasta_file)
+
+
+def test_load_fasta_file(fasta_path):
+    # Load the FASTA file
+    seq_records = fasta_file_io.load_fasta_file(fasta_path)
+
+    # Check that the correct number of records was loaded
+    assert len(seq_records) == 2
+
+    # Check that the IDs and sequences were loaded correctly
+    assert seq_records[0].id == "seq1"
+    assert seq_records[0].seq == "ATGC"
+    assert seq_records[1].id == "seq2"
+    assert seq_records[1].seq == "CGTA"
+
+
+def test_write_fasta_file(fasta_path):
+    # Create some SeqRecord objects
+    seq_records = [
+        fasta_file_io.SeqRecord(id="seq1", seq="ATGC"),
+        fasta_file_io.SeqRecord(id="seq2", seq="CGTA"),
+    ]
+
+    # Write the SeqRecord objects to a FASTA file
+    output_path = os.path.join(os.path.dirname(fasta_path), "output.fasta")
+    fasta_file_io.write_fasta_file(seq_records, output_path)
+
+    # Load the written FASTA file
+    written_seq_records = fasta_file_io.load_fasta_file(output_path)
+
+    # Check that the correct number of records was written
+    assert len(written_seq_records) == 2
+
+    # Check that the IDs and sequences were written correctly
+    assert written_seq_records[0].id == "seq1"
+    assert written_seq_records[0].seq == "ATGC"
+    assert written_seq_records[1].id == "seq2"
+    assert written_seq_records[1].seq == "CGTA"
+
+
+def test_load_fasta_file_raises_error_for_nonexistent_file():
+    with pytest.raises(FileNotFoundError):
+        fasta_file_io.load_fasta_file("nonexistent.fasta")
+
+
+def test_write_fasta_file_raises_error_for_invalid_output_path():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        seq_records = [
+            fasta_file_io.SeqRecord(id="seq1", seq="ATGC"),
+            fasta_file_io.SeqRecord(id="seq2", seq="CGTA"),
+        ]
+        with pytest.raises(IsADirectoryError):
+            fasta_file_io.write_fasta_file(seq_records, temp_dir)
+
+
+def test_hash_sequence_id():
+    # Test hash function with a known input and output
+    assert fasta_file_io.hash_sequence_id("test") == "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
+
+
+def test_encode_faa_ids(fasta_path):
+    # Run the encode_faa_ids function on the test FASTA file
+    faa_hashed_ids_path, hash_lookup_dict = fasta_file_io.encode_faa_ids(fasta_path)
+
+    # Check that the output files were created
+    assert os.path.exists(faa_hashed_ids_path)
+
+    # Check that the hash lookup dictionary was created correctly
+    assert len(hash_lookup_dict) == 2
+    assert "seq1" in hash_lookup_dict.values()
+    assert "seq2" in hash_lookup_dict.values()
+
+    # Load the hashed ID FASTA file
+    seq_records = fasta_file_io.load_fasta_file(faa_hashed_ids_path)
+
+    # Check that the sequence IDs were encoded correctly
+    assert seq_records[0].id != "seq1"
+    assert seq_records[1].id != "seq2"
+
+    # Check that the hash lookup dictionary can be used to get the original IDs
+    assert hash_lookup_dict[seq_records[0].id] == "seq1"
+    assert hash_lookup_dict[seq_records[1].id] == "seq2"
+
+
+def test_encode_faa_ids_raises_error_for_nonexistent_file():
+    with pytest.raises(FileNotFoundError):
+        fasta_file_io.encode_faa_ids("nonexistent.fasta")
