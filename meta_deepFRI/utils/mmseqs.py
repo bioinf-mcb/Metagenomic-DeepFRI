386c3530df5cfc83d922eb79ebe4365bf63dd4e3
valentynbez <walentyn.bzsz@gmail.com>
2023-04-11 21:21:15 +0200

    fix: remove hashing

--- meta_deepFRI/utils/mmseqs.py
+++ meta_deepFRI/utils/mmseqs.py
@@ -1,11 +1,6 @@
-import hashlib
 import pathlib
 import tempfile
-
-from typing import Tuple
-
 import pandas as pd
-from pysam.libcfaidx import FastxFile
 
 from meta_deepFRI.config.names import MERGED_SEQUENCES, TARGET_MMSEQS_DB_NAME, MMSEQS_SEARCH_RESULTS
 from meta_deepFRI.utils.utils import run_command, merge_files_binary
@@ -14,45 +9,6 @@ MMSEQS_COLUMN_NAMES = [
     "query", "target", "identity", "alignment_length", "mismatches", "gap_openings", "query_start", "query_end",
     "target_start", "target_end", "e_value", "bit_score"
 ]
-
-
-def hash_sequence_id(sequence: str) -> str:
-    """Return the SHA256 encoding of protein sequence
-
-    Args:
-        sequence (str): Aminoacid sequence of the protein.
-
-    Returns:
-        SHA256 encoding of the protein sequence.
-    """
-    return hashlib.sha256(bytes(sequence, encoding='utf-8')).hexdigest()
-
-
-def encode_faa_ids(input_file: pathlib.Path, output_file: pathlib.Path) -> Tuple[str, dict]:
-    """
-    Encodes multiline FASTA file IDs with SHA256 encoding.
-
-    Args:
-        input_file (pathlib.Path): Path to a fasta file.
-        output_file (pathlib.Path): Path to the output file.
-
-    Returns:
-        Path to the new file with ID and respective SHA256-encoded sequences and
-        a dictionary where keys represent sequence ID, and values SHA256 encoding.
-    """
-
-    hash_lookup_dict = {}
-    with FastxFile(input_file) as fasta_in, open(output_file, "w", encoding="utf-8") as fasta_out:
-        for fasta_entry in fasta_in:
-            seq_id_hash = hash_sequence_id(fasta_entry.name)
-            while seq_id_hash in hash_lookup_dict.keys():
-                seq_id_hash = hash_sequence_id(fasta_entry.name + "1")
-            hash_lookup_dict[seq_id_hash] = fasta_entry.name
-            fasta_entry.name = seq_id_hash
-            fasta_entry.comment = ""
-            fasta_out.write(str(fasta_entry) + "\n")
-
-    return output_file, hash_lookup_dict
 
 
 def createdb(sequences_file, db_path):
@@ -121,24 +77,17 @@ def run_mmseqs_search(query_file: pathlib.Path, target_db: pathlib.Path, output_
         return pd.read_csv(output_file, sep="\t", names=MMSEQS_COLUMN_NAMES)
 
     else:
-        # Create hashed IDs for query sequences
-        hashed_file = output_path / query_file.stem + ".hashed_ids.faa"
-        faa_hashed_ids_path, hash_lookup_dict = encode_faa_ids(query_file, hashed_file)
-        # Create MMSeqs2 database from query sequences.
         query_db = output_path / 'queryDB'
-        createdb(faa_hashed_ids_path, query_db)
+        createdb(query_file, query_db)
 
         result_db = output_path / 'search_resultDB'
         search(query_db, target_db, result_db)
 
         # Convert results to tabular format
-        hashed_output_file = str(output_file) + ".hashed_ids"
-        convertalis(query_db, target_db, result_db, hashed_output_file)
+        convertalis(query_db, target_db, result_db, output_file)
 
-        # Decode hashed IDs
-        output = pd.read_csv(hashed_output_file, sep="\t", names=MMSEQS_COLUMN_NAMES)
-        output["query"] = output["query"].map(hash_lookup_dict)
+        output = pd.read_csv(output_file, sep="\t", names=MMSEQS_COLUMN_NAMES)
 
-        output.to_csv(output_file, sep="\t", index=False, header=False)
+        output.to_csv(output_file, sep="\t")
 
         return output
