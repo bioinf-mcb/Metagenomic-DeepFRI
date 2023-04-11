67cf4aea366d75bd0ff5a3f47ae60d7e25f75815
valentynbez <walentyn.bzsz@gmail.com>
2023-04-11 09:22:44 +0200

    fix: hard-coded dict - try to remove biopython dep

--- meta_deepFRI/utils/bio_utils.py
+++ meta_deepFRI/utils/bio_utils.py
@@ -1,6 +1,38 @@
-from Bio import SeqUtils
+# copied from Biopython to remove dependency
+
+protein_letters_1to3 = {
+    "A": "Ala",
+    "C": "Cys",
+    "D": "Asp",
+    "E": "Glu",
+    "F": "Phe",
+    "G": "Gly",
+    "H": "His",
+    "I": "Ile",
+    "K": "Lys",
+    "L": "Leu",
+    "M": "Met",
+    "N": "Asn",
+    "P": "Pro",
+    "Q": "Gln",
+    "R": "Arg",
+    "S": "Ser",
+    "T": "Thr",
+    "V": "Val",
+    "W": "Trp",
+    "Y": "Tyr",
+}
+protein_letters_1to3_extended = {
+    **protein_letters_1to3,
+    **{"B": "Asx", "X": "Xaa", "Z": "Glx", "J": "Xle", "U": "Sec", "O": "Pyl"},
+}
+
+protein_letters_3to1 = {value: key for key, value in protein_letters_1to3.items()}
+protein_letters_3to1_extended = {
+    value: key for key, value in protein_letters_1to3_extended.items()
+}
 
 PROTEIN_LETTERS = dict()
-for k, v in SeqUtils.IUPACData.protein_letters_3to1_extended.items():
+for k, v in protein_letters_3to1_extended.items():
     PROTEIN_LETTERS[str.upper(k)] = v
 PROTEIN_LETTERS["UNK"] = "X"
