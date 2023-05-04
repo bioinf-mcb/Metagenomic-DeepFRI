e77ff1a683358461490765c897fc502358871405
valentynbez <walentyn.bzsz@gmail.com>
2023-05-04 09:27:35 +0200

    fix: pattern in config

--- mDeepFRI/__init__.py
+++ mDeepFRI/__init__.py
@@ -1,24 +1,38 @@
+import os
+
 __version__ = "2.0.0"
-model_links = [
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepCNN-MERGED_bp.onnx",
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepCNN-MERGED_cc.onnx",
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepCNN-MERGED_mf.onnx",
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepCNN-MERGED_ec.onnx",
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_bp.onnx",
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_cc.onnx",
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_ec.onnx",
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_mf.onnx",
+
+repo_link = "https://huggingface.co/crusher083/mDeepFRI/resolve/main"
+
+model_names = [
+    "DeepCNN-MERGED_bp.onnx",
+    "DeepCNN-MERGED_cc.onnx",
+    "DeepCNN-MERGED_mf.onnx",
+    "DeepCNN-MERGED_ec.onnx",
+    "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_bp.onnx",
+    "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_cc.onnx",
+    "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_ec.onnx",
+    "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_mf.onnx",
 ]
+
+model_links = [
+    os.path.join(repo_link, model_name) for model_name in model_names
+]
+
+config_names = [
+    "DeepCNN-MERGED_bp_model_params.json",
+    "DeepCNN-MERGED_cc_model_params.json",
+    "DeepCNN-MERGED_mf_model_params.json",
+    "DeepCNN-MERGED_ec_model_params.json",
+    "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_bp_model_params.json",
+    "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_cc_model_params.json",
+    "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_ec_model_params.json",
+    "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_mf_model_params.json",
+    "model_config.json"
+]
+
 config_links = [
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepCNN-MERGED_bp_model_params.json",
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepCNN-MERGED_cc_model_params.json",
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepCNN-MERGED_mf_model_params.json",
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepCNN-MERGED_ec_model_params.json",
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_bp_model_params.json",
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_cc_model_params.json",
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_ec_model_params.json",
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_mf_model_params.json",
-    "https://huggingface.co/crusher083/mDeepFRI/resolve/main/model_config.json"
+    os.path.join(repo_link, config_name) for config_name in config_names
 ]
 
 TARGET_MMSEQS_DB_NAME = "targetDB"
