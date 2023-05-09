c2414cbe7a3471c473a18aee289d2f2736bb6c3a
valentynbez <walentyn.bzsz@gmail.com>
2023-05-09 09:44:45 +0200

    fix: remove cmake compile

--- setup.py
+++ setup.py
@@ -16,53 +16,6 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-# class CMakeExtension(Extension):
-
-#     def __init__(self, name):
-#         # don't invoke the original build_ext for this special extension
-#         super().__init__(name, sources=[])
-
-# class build_ext(_build_ext):
-
-#     def run(self):
-#         for ext in self.extensions:
-#             self.build_cmake(ext)
-#         super().run()
-
-#     def build_cmake(self, ext):
-#         cwd = pathlib.Path().absolute()
-
-#         # these dirs will be created in build_py, so if you don't have
-#         # any python sources to bundle, the dirs will be missing
-#         build_temp = pathlib.Path(self.build_temp)
-#         build_temp.mkdir(parents=True, exist_ok=True)
-#         extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
-#         extdir.mkdir(parents=True, exist_ok=True)
-
-#         # example of cmake args
-#         python_version = f"{sys.version_info.major}.{sys.version_info.minor}"
-#         python_include_path = str(pathlib.Path(
-#             sys.executable).parent.parent.absolute()) + f'/include/python{python_version}/'
-
-#         config = 'Debug' if self.debug else 'Release'
-#         cmake_args = [
-#             '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(extdir.parent.absolute()),
-#             '-DPY_INCLUDE_PATH=' + python_include_path,
-#             '-DCMAKE_BUILD_TYPE=' + config,
-#         ]
-
-#         # example of build args
-#         build_args = ['--config', config, '--', '-j4']
-
-#         os.chdir(str(build_temp))
-#         self.spawn(['cmake', str(cwd)] + cmake_args)
-#         if not self.dry_run:
-#             self.spawn(['cmake', '--build', '.'] + build_args)
-#         # Troubleshooting: if fail on line above then delete all possible
-#         # temporary CMake files including "CMakeCache.txt" in top level dir.
-#         os.chdir(str(cwd))
-
-
 class build_ext(_build_ext):
     def run(self):
         if isinstance(cythonize, ImportError):
