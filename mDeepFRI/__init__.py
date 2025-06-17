53d149cbc8ee2881d8a3979c34aa24c9d0b3f48b
valentynbez <walentyn.bzsz@gmail.com>
2025-06-17 11:39:39 +0200

    refractor: reduce code in init

--- mDeepFRI/__init__.py
+++ mDeepFRI/__init__.py
@@ -15,114 +15,32 @@ DEEPFRI_MODES = {
     "ec": "Enzyme Commission"
 }
 
-cnn_model_links = {
-    "bp": {
-        "model": os.path.join(repo_url, "DeepCNN-MERGED_bp.onnx"),
-        "config": os.path.join(repo_url, "DeepCNN-MERGED_bp_model_params.json")
-    },
-    "cc": {
-        "model": os.path.join(repo_url, "DeepCNN-MERGED_cc.onnx"),
-        "config": os.path.join(repo_url, "DeepCNN-MERGED_cc_model_params.json")
-    },
-    "mf": {
-        "model": os.path.join(repo_url, "DeepCNN-MERGED_mf.onnx"),
-        "config": os.path.join(repo_url, "DeepCNN-MERGED_mf_model_params.json")
-    },
-    "ec": {
-        "model": os.path.join(repo_url, "DeepCNN-MERGED_ec.onnx"),
-        "config": os.path.join(repo_url, "DeepCNN-MERGED_ec_model_params.json")
-    },
-}
+
+def make_links(repo_url, prefix, terms):
+    return {
+        term: {
+            "model": os.path.join(repo_url, f"{prefix}_{term}.onnx"),
+            "config": os.path.join(repo_url,
+                                   f"{prefix}_{term}_model_params.json"),
+        }
+        for term in terms
+    }
+
+
+terms = ["bp", "cc", "mf", "ec"]
+
+cnn_model_links = make_links(repo_url, "DeepCNN-MERGED", terms)
 
 gcn_model_links = {
-    "1.0": {
-        "bp": {
-            "model":
-            os.path.join(
-                repo_url,
-                "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_bp.onnx"
-            ),
-            "config":
-            os.path.join(
-                repo_url,
-                "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_bp_model_params.json"
-            )
-        },
-        "cc": {
-            "model":
-            os.path.join(
-                repo_url,
-                "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_cc.onnx"
-            ),
-            "config":
-            os.path.join(
-                repo_url,
-                "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_cc_model_params.json"
-            )
-        },
-        "mf": {
-            "model":
-            os.path.join(
-                repo_url,
-                "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_mf.onnx"
-            ),
-            "config":
-            os.path.join(
-                repo_url,
-                "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_mf_model_params.json"
-            )
-        },
-        "ec": {
-            "model":
-            os.path.join(
-                repo_url,
-                "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_ec.onnx"
-            ),
-            "config":
-            os.path.join(
-                repo_url,
-                "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_ec_model_params.json"
-            )
-        }
-    },
-    "1.1": {
-        "bp": {
-            "model":
-            os.path.join(
-                repo_url,
-                "DeepFRI-UNIPROT_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_ext_desc_bp.onnx"
-            ),
-            "config":
-            os.path.join(
-                repo_url,
-                "DeepFRI-UNIPROT_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_ext_desc_bp_model_params.json"
-            )
-        },
-        "cc": {
-            "model":
-            os.path.join(
-                repo_url,
-                "DeepFRI-UNIPROT_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_ext_desc_cc.onnx"
-            ),
-            "config":
-            os.path.join(
-                repo_url,
-                "DeepFRI-UNIPROT_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_ext_desc_cc_model_params.json"
-            )
-        },
-        "mf": {
-            "model":
-            os.path.join(
-                repo_url,
-                "DeepFRI-UNIPROT_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_ext_desc_mf.onnx"
-            ),
-            "config":
-            os.path.join(
-                repo_url,
-                "DeepFRI-UNIPROT_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_ext_desc_mf_model_params.json"
-            )
-        },
-    }
+    "1.0":
+    make_links(repo_url,
+               "DeepFRI-MERGED_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0",
+               terms),
+    "1.1":
+    make_links(
+        repo_url,
+        "DeepFRI-UNIPROT_GraphConv_gcd_512-512-512_fcd_1024_ca_10.0_ext_desc",
+        ["bp", "cc", "mf"])
 }
 
 BAR_FORMAT = "{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}], {rate_fmt}{postfix}"
